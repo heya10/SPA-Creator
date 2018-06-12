c642ffdb0becd609f2442faade94f46377f8b44f
heya10 <igor.gielzak@gmail.com>
2018-06-12 19:03:44 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SPA-Creator
 Simple Single Page Application Creator
+
+It should simplify a little bit creating and managing SPA.
+
+Written in:
+Java | Spring Boot | MongoDB | Bootstrap | a lot more
---
+++ src/main/java/pl/gielzak/spa_creator/SpaCreatorApplication.java
@@ -0,0 +1,12 @@
+package pl.gielzak.spa_creator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaCreatorApplication.class, args);
+	}
+}
